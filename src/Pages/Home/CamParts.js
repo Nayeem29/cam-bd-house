56ced6a03660935f9e87d898a1d3d0812f448f58
Nayeem29 <nayeembracu29@gmail.com>
2022-05-22 03:54:04 +0600

    camera parts displayed in homepage

--- src/Pages/Home/CamParts.js
+++ src/Pages/Home/CamParts.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Products from './Products';
 
 const CamParts = () => {
+  const [camparts, setCamparts] = useState([]);
+  useEffect(() => {
+    fetch('parts.json')
+      .then(res => res.json())
+      .then(data => setCamparts(data))
+  }, [])
   return (
-    <div className=''>
-      <h1 className='text-4xl font-bold text-[#525252]'>Our Featured Products</h1>
-      <div>
-
+    <div className='mb-5 ml-2 lg:mx-5'>
+      <h1 className='text-4xl font-bold text-[#525252] mb-12'>Our Featured Products</h1>
+      <hr className=' h-2 w-full' />
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+        {
+          camparts.map(cam => <Products
+            key={cam._id}
+            cam={cam}
+          ></Products>)
+        }
       </div>
     </div>
   );
---
+++ src/Pages/Home/Products.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Products = ({ cam }) => {
+  const { name, image, description, minOrderQuantity, available, price } = cam;
+  return (
+    <div className="hero ">
+      <div className="hero-content flex-col lg:flex-row">
+        <div>
+          <h1 className="text-2xl text-[#525252] font-bold mb-3">{name}</h1>
+          <img src={image} className="max-w-sm rounded-lg shadow-2xl" alt='' />
+        </div>
+        <div className='align-middle'>
+
+          <div className='sm:ml-2 ml-0'>
+            <h1 className="text-base font-normal">Min Order:
+              <span className='text-xl font-medium'>{minOrderQuantity}</span>
+            </h1>
+            <h1 className="text-base font-normal">Available:
+              <span className='text-xl font-medium'>{available}</span>
+            </h1>
+            <h1 className="text-base font-normal">
+              Price: <span className='text-xl font-medium'>${price} per unit</span>
+            </h1>
+          </div>
+          <p className="py-6 text-justify">
+            {description.length > 100 ? description.slice(0, 100) + '...' : description}
+          </p>
+          <button className="btn btn-primary">Order Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
